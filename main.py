72ef9f55f4593e5e95cacae90f97005deaf23dff
Shokhrukh Tukhtasinov <shoxruxbekbest@gmail.com>
2024-01-31 20:56:47 +0100

    new4

--- main.py
+++ main.py
@@ -48,6 +48,20 @@ def process_coins():
     total += int(input("How many pennies? ")) * 0.01
     return total
 
+def is_transaction_successful(money_recieved, drink_cost):
+    if money_recieved >= drink_cost:
+        change = round(money_recieved - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enought money. Money refunded")
+        return False
+
+
+
+
     
 
 
